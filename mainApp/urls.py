f54371c83b1ca8b9b18d0d891ea4ff6b189bdde1
Abheelash-Mishra <abheelashmishra@gmail.com>
2023-03-15 00:04:49 +0530

    Added delete post functionality

--- mainApp/urls.py
+++ mainApp/urls.py
@@ -1,6 +1,9 @@
 from django.urls import path
-from .views import SocialPosts
+from .views import SocialPosts,DeletePost
 
 urlpatterns = [
     path("", SocialPosts.as_view(), name="content_list"),
+    # path('edit/<int:post_id>/', edit_post, name='edit_post'),
+    path("post/delete/<int:pk>", DeletePost.as_view(), name="delete_post"),
+
 ]
--- mainApp/views.py
+++ mainApp/views.py
@@ -1,10 +1,37 @@
 from django.shortcuts import render
+from django.urls import reverse_lazy
 from django.views import View
 from .models import Post
 from .forms import Post_Entry, Comment_Entry
+from django.shortcuts import redirect
+from django.views.generic import UpdateView, DeleteView
 
 # Create your views here.
 
+# class EditPost(UpdateView):
+#     model = Post
+#     fields = ["body"]
+#     template_name =  "edit_post.html"
+#
+#     def get_success_url(self):
+#
+
+# def post_view(request, post_id):
+#     post = Post.objects.get(id=post_id)
+#     return render(request, 'content_list.html', {'post': post})
+# def edit_post(request, post_id):
+#     post = Post.objects.get(id=post_id)
+#     if request.method == 'POST':
+#         post.body = request.POST.get('body')
+#         post.save()
+#         return redirect(post_view, post_id=post.id)
+#     return render(request, 'edit_post.html', {'post': post})
+
+
+class DeletePost(DeleteView):
+    model = Post
+    template_name = "delete_post.html"
+    success_url = reverse_lazy("content_list")
 class SocialPosts(View):
     def get(self, request, *args, **kwargs):
         content_posts = Post.objects.all().order_by("-created")
